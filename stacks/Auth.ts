ea5d46bd67a7275ca4f36ff29b13c910bb500a9e
Matthew Wyskiel <developer@mattwyskiel.com>
2022-08-20 18:19:29 -0400

    Remove custom domain for simplicity's sake

--- stacks/Auth.ts
+++ stacks/Auth.ts
@@ -1,5 +1,4 @@
 import { StackContext } from '@serverless-stack/resources';
-import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 import { UserPool } from 'aws-cdk-lib/aws-cognito';
 
 export function AuthStack({ stack }: StackContext) {
@@ -12,21 +11,9 @@ export function AuthStack({ stack }: StackContext) {
     },
   });
   const upDomain = userPool.addDomain('UserPoolDomain', {
-    customDomain: !process.env.IS_LOCAL
-      ? {
-          domainName: 'auth.whiskey.mattwyskiel.com',
-          certificate: Certificate.fromCertificateArn(
-            stack,
-            'Certificate',
-            'arn:aws:acm:us-east-1:662292074719:certificate/7af85e3e-fe4a-46db-bada-cd4421ff7a6a'
-          ),
-        }
-      : undefined,
-    cognitoDomain: process.env.IS_LOCAL
-      ? {
-          domainPrefix: 'whiskey-apps-auth',
-        }
-      : undefined,
+    cognitoDomain: {
+      domainPrefix: 'whiskey-apps-auth',
+    },
   });
 
   stack.addOutputs({
