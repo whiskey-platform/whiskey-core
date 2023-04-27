beb2859480139181704615ccce478dfd207af365
Matthew Wyskiel <developer@mattwyskiel.com>
2023-04-26 22:04:10 -0400

    [api-infra] send outputs to SSM

--- api-infra/stacks/MyStack.ts
+++ api-infra/stacks/MyStack.ts
@@ -15,7 +15,6 @@ export function Infra({ stack, app }: StackContext) {
       : undefined,
   });
   stack.addOutputs({
-    domainName: api.cdk.domainName?.name,
     regionalDomainName: api.cdk.domainName?.regionalDomainName,
     regionalHostedZoneId: api.cdk.domainName?.regionalHostedZoneId,
     certificateArn: api.cdk.certificate?.certificateArn,
