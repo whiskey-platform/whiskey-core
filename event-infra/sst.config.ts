e692e92bf948394eb14183ff5a69bd4ce2bdcdb3
Matthew Wyskiel <matt@mattwyskiel.com>
2024-04-23 15:01:30 -0400

    [event-infra] create event bus

---
+++ event-infra/sst.config.ts
@@ -0,0 +1,16 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "event-infra",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    const eventBus = new aws.cloudwatch.EventBus("EventBus", {
+      name: "whiskey-event-bus",
+    });
+  },
+});
